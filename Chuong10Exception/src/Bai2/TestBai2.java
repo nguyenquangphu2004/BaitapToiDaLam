29fb43761bf0b5de609f74efbb6154d568606655
nguyenquangphu2004 <quangphu2060@gmail.com>
2022-12-12 22:45:59 +0700

    Bai tap ve Exception 1

--- Chuong10Exception/src/Bai2/TestBai2.java
+++ Chuong10Exception/src/Bai2/TestBai2.java
@@ -4,10 +4,10 @@ public class TestBai2 {
     public static void main(String[] args) throws InvalidAmoutException {
 
         AccountBank accountBank1 = new AccountBank(
-                "9294124142", "8999005022004",
+                "9294124142", "8352385",
                 "NGUYEN QUANG PHU",1700000);
         AccountBank accountBank2 = new AccountBank(
-                "05491242142","0377018551",
+                "05491242142","94248223",
                 "TRAN THI TOAN",5000000);
 //Thực hiện chuyển tiền không hợp lệ;
         System.out.println("Thông tin tài khoản trước khi chuyển: ");
